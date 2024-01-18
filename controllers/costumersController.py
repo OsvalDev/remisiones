36ee07efb084b5c45cac53c5f153099cd33f2b27
OsvalDev <osvaworkdev@gmail.com>
2024-01-18 13:05:13 -0600

    Fix,update requirements

--- controllers/costumersController.py
+++ controllers/costumersController.py
@@ -3,8 +3,8 @@ from sqlalchemy import text
 def syncClientDb(dbSqlServer, mysql):
     try:        
         raw_sql = text('SELECT * FROM CatClientes')
-        # result = dbSqlServer.session.execute(raw_sql)        
-        #costumerListSqlServer = [row for row in result]
+        result = dbSqlServer.session.execute(raw_sql)
+        costumerListSqlServer = [row for row in result]
 
         cur = mysql.connection.cursor()
         cur.execute('''        
@@ -13,20 +13,20 @@ def syncClientDb(dbSqlServer, mysql):
         ''' )
         costumerListMysql = cur.fetchall()        
 
-        # keyCostumers = [key[0] for key in costumerListMysql]
-        # inMysql = [row for row in costumerListSqlServer if row[0] in keyCostumers]
-        # notInMysql = [row for row in costumerListSqlServer if not row[0] in keyCostumers]
-        # modifiedList = [(row[1], row[2], row[0]) for row in inMysql]
+        keyCostumers = [key[0] for key in costumerListMysql]
+        inMysql = [row for row in costumerListSqlServer if row[0] in keyCostumers]
+        notInMysql = [row for row in costumerListSqlServer if not row[0] in keyCostumers]
+        modifiedList = [(row[1], row[2], row[0]) for row in inMysql]
 
-        # sqlQuery = "INSERT INTO CLIENTE (id, clave, nombre ) VALUES (%s, %s, %s)"
-        # cur.executemany(sqlQuery, notInMysql)
-        # mysql.connection.commit()
+        sqlQuery = "INSERT INTO CLIENTE (id, clave, nombre ) VALUES (%s, %s, %s)"
+        cur.executemany(sqlQuery, notInMysql)
+        mysql.connection.commit()
         
-        # sqlQuery = "UPDATE CLIENTE SET clave = %s, nombre = %s WHERE id = %s"
-        # cur.executemany(sqlQuery, modifiedList)
-        # mysql.connection.commit()
+        sqlQuery = "UPDATE CLIENTE SET clave = %s, nombre = %s WHERE id = %s"
+        cur.executemany(sqlQuery, modifiedList)
+        mysql.connection.commit()
         
-        # cur.close()        
+        cur.close()        
         return {'result' : 'success', 'msg' : 'Base de datos sincronizada correctamente'}
 
     except Exception as e:
