4789130f421b1376be27e899f4a7ea061cb661a7
OsvalDev <osvaworkdev@gmail.com>
2024-02-18 00:23:24 -0600

    Fix: register parcel delivery

--- controllers/followController.py
+++ controllers/followController.py
@@ -127,21 +127,21 @@ def registerDelivery (mysql, data):
     try:
         cur.execute('''INSERT INTO PROCESO (accion, numRemision, numCompra, usuario, fechaConcluido)
                             VALUES (%s, %s, %s, %s , %s)''',
-                        ('Entrega', data['numRemision'], data['numCompra'], data['usuario'], data['date']))
+                        ('Entrega', data['numRemision'], data['numCompra'], data['user'], data['date']))
         mysql.connection.commit()
 
         cur.execute('''SELECT id FROM PROCESO WHERE accion = %s and numRemision = %s and numCompra = %s''',
                         ('Entrega', data['numRemision'], data['numCompra']))
         idProcess = cur.fetchone()[0]
-
-        cur.execute('''INSERT INTO NOTIFICACIONENTREGA 
-                        VALUES (%s, %s, %s, )''',
-                    (idProcess, data['usuario'], data['parcelName']))
+        
+        cur.execute('''INSERT INTO NOTIFICANTEENTREGA VALUES (%s, %s, %s )''',
+                    (idProcess, data['user'], data['parcelName']))
         mysql.connection.commit()
 
-        cur.execute('''UPDATE REMISION SET estatus = 5 WHERE numRemision = %s and numCompra = %s
-            ''', (data['numRemission'], data['numCompra']))
+        cur.execute('''UPDATE REMISION SET estatus = 5 WHERE numRemision = %s and numCompra = %s''',
+                     (data['numRemision'], data['numCompra']))
         mysql.connection.commit()
+        
         return True
     except Exception as e:
         print(e)
